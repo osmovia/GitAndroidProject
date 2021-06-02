d140091bbff6cb86725cbf18d27bc6fb28a33e14
Viacheslav Osmolovskyi <osmovia@gmail.com>
2021-06-02 15:38:36 +0300

    add fun TOAST

--- app/src/main/java/com/example/gitandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/gitandroidproject/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.gitandroidproject
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun toastMe (view: View) {
+        val myToast = Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT)
+
+        myToast.show()
     }
 }
