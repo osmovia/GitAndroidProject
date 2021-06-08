e820761d95f95cf6a7159490463d920159a51845
Viacheslav Osmolovskyi <osmovia@gmail.com>
2021-06-08 15:01:38 +0300

    new random fun

--- app/src/main/java/com/example/gitandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/gitandroidproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gitandroidproject
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
@@ -12,17 +13,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun toastMe (view: View) {
+    fun toast (view: View) {
         val myToast = Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT)
 
         myToast.show()
     }
 
-    fun countMe(view: View) {
-        val countString = findViewById<TextView>(R.id.textView).text.toString()
-        var count: Int = countString.toInt()
+    fun count (view: View) {
+        val textView = findViewById<TextView>(R.id.textView)
+        val countString = textView.text.toString()
+        var count: Int = Integer.parseInt(countString)
         count++
 
-        findViewById<TextView>(R.id.textView).text = count.toString()
+        textView.text = count.toString()
+    }
+
+    fun random (view: View) {
+        val randomIntent = Intent(this,SecondActivity:: class.java)
+        val textView = findViewById<TextView>(R.id.textView)
+        val countString = textView.text.toString()
+        val count = Integer.parseInt(countString)
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT,count)
+        startActivity(randomIntent)
     }
 }
--- app/src/main/java/com/example/gitandroidproject/SecondActivity.kt
+++ app/src/main/java/com/example/gitandroidproject/SecondActivity.kt
@@ -1,11 +1,32 @@
 package com.example.gitandroidproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class SecondActivity : AppCompatActivity() {
+
+    companion object {
+        const val TOTAL_COUNT = "total count"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        showRandomNumber()
+    }
+
+    fun showRandomNumber () {
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+        val random = Random
+        var randomInt = 0
+
+        if(count > 0) {
+            randomInt = random.nextInt(count +1)
+        }
+        findViewById<TextView>(R.id.textViewRandom).text = randomInt.toString()
+
+        findViewById<TextView>(R.id.textViewLabel).text = getString(R.string.random_heading, count)
+
     }
 }
